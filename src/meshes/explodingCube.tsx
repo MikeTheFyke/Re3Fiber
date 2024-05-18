91a50cf2a6fe7830a9e58a96fbe78c883fed12b8
MikeTheFyke <mikefyke@hotmail.com>
2024-05-18 13:56:40 -0400

    Added onclick and hover events to exploding cube

--- src/meshes/explodingCube.tsx
+++ src/meshes/explodingCube.tsx
@@ -1,8 +1,27 @@
 import { Center, useAnimations, useGLTF } from "@react-three/drei";
-import React, { Suspense } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useMotionValue, useSpring } from "framer-motion";
+import React, { Suspense, useRef } from "react";
+import { AnimationAction, Group, MathUtils } from "three";
 
 const ExplodingCube = () => {
 	useGLTF("./explodeCube.glb");
+
+	const motionVal = useMotionValue(0);
+	const spring = useSpring(motionVal, { damping: 1, stiffness: 20 });
+
+	const groupRef = useRef<Group>(null);
+
+	useFrame(({ camera }) => {
+		if (groupRef.current) {
+			groupRef.current?.rotateY(MathUtils.degToRad(0.2));
+		}
+		Object.keys(actions).forEach((key) => {
+			const action = actions[key] as AnimationAction;
+			action.play().paused = true;
+			action.time = spring.get();
+		});
+	});
 
 	const { nodes, animations, scene } = useGLTF("./explodeCube.glb");
 	const { actions } = useAnimations(animations, scene);
@@ -12,7 +31,13 @@ const ExplodingCube = () => {
 			<directionalLight position={[-5, -5, 5]} intensity={2} />
 			<Suspense fallback={null}>
 				<Center>
-					<group>
+					<group
+						onPointerUp={() => motionVal.set(0)}
+						onPointerDown={() => motionVal.set(1)}
+						onPointerOver={() => motionVal.set(1)}
+						onPointerLeave={() => motionVal.set(0)}
+						ref={groupRef}
+					>
 						<primitive object={scene} />
 					</group>
 				</Center>
